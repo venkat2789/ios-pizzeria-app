2713867699acc4f53c24d3708880e9cc626e0121
venkat2789 <venkatachalapathy.27@gmail.com>
2022-05-30 12:56:45 -0400

    Added Ingredeints as DisclosureGroup and CrustType as Picker

--- Pizzeria/Views/DetailView.swift
+++ Pizzeria/Views/DetailView.swift
@@ -9,11 +9,14 @@ import SwiftUI
 
 struct DetailView: View {
     let pizzaTypes = ["Pizza Margherita", "Greek Pizza", "Pizza Supreme", "Pizza California", "New York Pizza"]
+    let crustTypes = ["Normal", "Thin Crust"]
     
     @Environment(\.managedObjectContext) private var viewContext
     @EnvironmentObject var viewRouter: ViewRouter
     @State var selectedPizzaIndex = 1
     @State var numberOfSlices = 1
+    @State var crustType = 1
+    @State var isIngredientExpanded : Bool = true
     
     var body: some View {
         VStack{
@@ -37,36 +40,77 @@ struct DetailView: View {
                 }
                 .navigationTitle("Order Details")
             }
-            Button(action: {
-                let newOrder = PizzaOrder(context: viewContext)
-                newOrder.pizzaType = self.pizzaTypes[self.selectedPizzaIndex]
-                newOrder.orderStatus = .pending
-                newOrder.numberOfSlices = Int16(self.numberOfSlices)
-                newOrder.id = UUID()
-                
-                do {
-                    try viewContext.save()
-                    print("Order saved.")
-                    withAnimation(.default){
-                        //viewRouter.currentPage = .landing
-                        //This does not work as viewRouter still has landing value and it has not changed
-                    }
-                    
-                } catch {
-                    print(error.localizedDescription)
+            
+            
+            ScrollView{
+                DisclosureGroup("Ingredients", isExpanded: $isIngredientExpanded) {
+                    Text("These are the ingredients.These are the ingre dients. These are the ingredients. These are the ingredients.")
+                        .multilineTextAlignment(.leading)
+                        .foregroundColor(.black)
+                        .font(.caption)
                 }
-            }) {
-                Text("Add to Order")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(width: 150, height: 45)
-                    .background(Color.red)
-                    .cornerRadius(15.0)
+                .padding()
+                .foregroundColor(.black)
+                .accentColor(.red)
+                
+                NavigationView {
+                    Form {
+                        Section(header: Text("Pizza Type")) {
+                            Picker(selection: $selectedPizzaIndex, label: Text("Pizza Type")) {
+                                ForEach(0 ..< pizzaTypes.count, id:\.self) {
+                                    Text(self.pizzaTypes[$0]).tag($0)
+                                }
+                            }
+                            
+                            Stepper("\(numberOfSlices) Slices", value: $numberOfSlices, in: 1...12)
+                        }
+                        .navigationBarBackButtonHidden(true)
+                        Section(header: Text("Crust")) {
+                            Picker("Crust", selection: $crustType) {
+                                ForEach(0 ..< crustTypes.count, id:\.self) {
+                                    Text(self.crustTypes[$0]).tag($0)
+                                }
+                            }
+                            .pickerStyle(.segmented)
+                        }
+                    }
+                    .navigationTitle("Customize")
+                    .navigationBarTitleDisplayMode(.inline)
+                }
+                .frame(height: 400, alignment: .center)
+                
+                Button(action: {
+                    let newOrder = PizzaOrder(context: viewContext)
+                    newOrder.pizzaType = self.pizzaTypes[self.selectedPizzaIndex]
+                    newOrder.orderStatus = .pending
+                    newOrder.numberOfSlices = Int16(self.numberOfSlices)
+                    newOrder.id = UUID()
+                    
+                    do {
+                        try viewContext.save()
+                        print("Order saved.")
+                        withAnimation(.default){
+                            //viewRouter.currentPage = .landing
+                            //This does not work as viewRouter still has landing value and it has not changed
+                        }
+                        
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                }) {
+                    Text("Add to Order")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 150, height: 45)
+                        .background(Color.red)
+                        .cornerRadius(15.0)
+                }
+                .padding()
             }
-            .padding()
         }
         .background(Color("LightGrayBackground"))
+        .navigationBarBackButtonHidden(true)
     }
 }
 
