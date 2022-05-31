1ea2c9d2881fc665752374166b8aeef1e1b5feeb
venkat2789 <venkatachalapathy.27@gmail.com>
2022-05-30 20:07:33 -0400

    Removed NavigationBar for CategoryHome and fixed alignment

--- Pizzeria/Views/Categories/CategoryHome.swift
+++ Pizzeria/Views/Categories/CategoryHome.swift
@@ -10,37 +10,51 @@ struct CategoryHome: View {
     @EnvironmentObject var viewRouter: ViewRouter
     @EnvironmentObject var modelData: ModelData
     @State private var showingProfile = false
-
+    
     var body: some View {
         NavigationView {
-            List {
-                Text("Select from our amazing categories of pizza below or view the full menu.")
-                    .padding(.top)
-                    .padding(.bottom)
-//                PageView(pages: modelData.features.map { FeatureCard(landmark: $0) })
-//                    .aspectRatio(3 / 2, contentMode: .fit)
-//                    .listRowInsets(EdgeInsets())
-
-                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
-                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
-                        .padding(.bottom)
+            VStack{
+                VStack(alignment: .leading, spacing: 10.0) {
+                    Text("Welcome,")
+                        .font(.title)
+                        .bold()
+                        .padding(.top, 20)
+                    
+                    Text("Select from our amazing categories of pizza below or view the full menu.")
+                        .font(.title3)
+                        .padding(.bottom, 10)
                 }
-                .listRowInsets(EdgeInsets())
+                .padding(.leading, 10)
+                .padding(.trailing, 10)
+                
+                List {
+                    //                PageView(pages: modelData.features.map { FeatureCard(landmark: $0) })
+                    //                    .aspectRatio(3 / 2, contentMode: .fit)
+                    //                    .listRowInsets(EdgeInsets())
+                    
+                    ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                        CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                            .padding(.bottom, 5)
+                            .padding(.top, 5)
+                    }
+                    .listRowInsets(EdgeInsets())
+                }
+                .listStyle(.inset)
+                //            .navigationTitle("Welcome,")
+                .navigationBarHidden(true)
+                .navigationBarBackButtonHidden(true)
+                //            .toolbar {
+                //                Button {
+                //                    showingProfile.toggle()
+                //                } label: {
+                //                    Label("User Profile", systemImage: "person.crop.circle")
+                //                }
+                //            }
+                //            .sheet(isPresented: $showingProfile) {
+                //               ProfileHost()
+                //                   .environmentObject(modelData)
+                //            }
             }
-            .listStyle(.inset)
-            .navigationTitle("Welcome,")
-            .navigationBarBackButtonHidden(true)
-//            .toolbar {
-//                Button {
-//                    showingProfile.toggle()
-//                } label: {
-//                    Label("User Profile", systemImage: "person.crop.circle")
-//                }
-//            }
-//            .sheet(isPresented: $showingProfile) {
-////                ProfileHost()
-////                    .environmentObject(modelData)
-//            }
         }
     }
 }
--- Pizzeria/Views/Categories/CategoryRow.swift
+++ Pizzeria/Views/Categories/CategoryRow.swift
@@ -15,7 +15,7 @@ struct CategoryRow: View {
             Text(categoryName)
                 .font(.headline)
                 .padding(.leading, 15)
-                .padding(.top, 5)
+                .padding(.top, 10)
             ScrollView(.horizontal, showsIndicators: false){
                 HStack(alignment: .top, spacing: 0) {
                     ForEach(items) { pizaa in
